fd14438b0399aeda5c95c53efc7d8ae773907049
MRBA86 <100731907+MRBA86@users.noreply.github.com>
2023-08-28 17:19:33 +0330

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 def index_view(request):
+    #context = {'languages':[], 'edgucations':[],'skills':[], 'projects':[], 'about':[], 'contact':[]}
     return render(request,'index.html')
 # Create your views here.
